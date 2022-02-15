cf583ce34d9ec60bcc5872c811731189efb9466f
Eduardo Vozniak <vozniak@msn.com>
2022-02-15 01:02:24 -0300

    Adding icons for bottom navigation

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -5,6 +5,9 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import AddTransaction from './components/Transaction/Transaction';
 import { RootSiblingParent } from 'react-native-root-siblings';
 import Settings from './components/Settings/Settings';
+import React from 'react';
+import { SimpleLineIcons, MaterialCommunityIcons, Feather, MaterialIcons } from '@expo/vector-icons'; 
+
 
 const Tab = createBottomTabNavigator();
 
@@ -13,11 +16,41 @@ export default function App() {
     <RootSiblingParent>     
       <NavigationContainer>    
         <Tab.Navigator>
-          <Tab.Screen name="Budget" component={HelloWorld} />
-          <Tab.Screen name="Account" component={HelloWorld} />
-          <Tab.Screen name="Transactions" component={AddTransaction} />
-          <Tab.Screen name="Report" component={HelloWorld} />
-          <Tab.Screen name="Settings" component={Settings} />        
+          <Tab.Screen name="Budget" component={HelloWorld} 
+              options={{
+                tabBarIcon: ({ color, size }) => (
+                  <SimpleLineIcons name="envelope" color={color} size={size} />
+                ),
+              }}
+          />
+          <Tab.Screen name="Account" component={HelloWorld} 
+              options={{
+                tabBarIcon: ({ color, size }) => (
+                  <MaterialCommunityIcons name="bank" color={color} size={size} />
+                ),
+              }}          
+          />
+          <Tab.Screen name="Transaction" component={AddTransaction} 
+              options={{
+                tabBarIcon: ({ color, size }) => (
+                  <Feather name="plus-circle" color={color} size={size} />
+                ),
+              }}           
+          />
+          <Tab.Screen name="Report" component={HelloWorld} 
+              options={{
+                tabBarIcon: ({ color, size }) => (
+                  <MaterialIcons name="bar-chart" color={color} size={size} />
+                ),
+              }}          
+          />
+          <Tab.Screen name="Settings" component={Settings} 
+              options={{
+                tabBarIcon: ({ color, size }) => (    
+                  <Feather name="settings" color={color} size={size} />
+                ),
+              }}          
+          />        
         </Tab.Navigator>
       </NavigationContainer>
     </RootSiblingParent> 
