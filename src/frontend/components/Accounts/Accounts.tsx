c47eb056879a3168fbad88a80726f40b7dca3a62
Eduardo Vozniak <vozniak@msn.com>
2022-02-20 01:05:12 -0300

    Formatting and dynamic accounts

--- src/frontend/components/Accounts/Accounts.tsx
+++ src/frontend/components/Accounts/Accounts.tsx
@@ -1,35 +1,61 @@
-import React from 'react';
-import { SectionList} from 'react-native';
-import { AddAccount, FlexViewHorizontal, FlexViewVertical, HeaderText, LeftText, RightText } from './Accounts.styled';
-import { useAppSelector } from '../../redux/hooks'
-
-export default function Accounts(){ 
-    const accounts = useAppSelector((state) => state.account.accounts);
-    const budgetAccounts = accounts.filter(acc => acc.type == 'Budget accounts');
-    const reserveAccounts = accounts.filter(acc => acc.type == 'Reserve');    
-    let budAcc = [];
-    let resAcc = [];
-    budgetAccounts.map(acc => {
-        budAcc.push({accountName: acc.name, accountBalance: acc.balance})
-    });
-    reserveAccounts.map(acc => {
-        resAcc.push({accountName: acc.name, accountBalance: acc.balance})
-    });    
-    let listData = [{title: 'Budget accounts', data: [...budAcc]}, {title: 'Reserve', data: [...resAcc]}];    
+import React from "react";
+import { SectionList } from "react-native";
+import {
+  AddAccount,
+  FlexViewHorizontal,
+  FlexViewVertical,
+  HeaderText,
+  LeftText,
+  RightText,
+} from "./Accounts.styled";
+import { useAppSelector, useAppDispatch } from "../../redux/hooks";
+import { addAccount } from "../../redux/accountSlice";
 
 
-    return (
-        <FlexViewVertical>
-            <SectionList
-            sections={listData}
-            renderItem={({item}) => <FlexViewHorizontal>
-                                        <LeftText >{item.accountName}</LeftText> 
-                                        <RightText>R${item.accountBalance}</RightText>
-                                    </FlexViewHorizontal>}
-            renderSectionHeader={({section}) => <HeaderText>{section.title}</HeaderText>}
-            keyExtractor={(item, index) => item.accountName}
-            />
-            <AddAccount title={'Add new account'}/>
-        </FlexViewVertical>
-    );
+export default function Accounts() {
+  const accounts = useAppSelector((state) => state.account.accounts);
+  const dispatch = useAppDispatch();
+
+  const budgetAccounts = accounts.filter(
+    (acc) => acc.type == "Budget accounts"
+  );
+  const reserveAccounts = accounts.filter((acc) => acc.type == "Reserve");
+  let budAcc = [];
+  let resAcc = [];
+  budgetAccounts.map((acc) => {
+    budAcc.push({ accountName: acc.name, accountBalance: acc.balance });
+  });
+  reserveAccounts.map((acc) => {
+    resAcc.push({ accountName: acc.name, accountBalance: acc.balance });
+  });
+  const listData = [
+    { title: "Budget accounts", data: [...budAcc] },
+    { title: "Reserve", data: [...resAcc] },
+  ];
+
+  function Teste(){
+      dispatch(addAccount({ key: "acc9", balance: 99999, name: 'Teste', type: 'Budget accounts'}))
+  }
+
+  return (
+    <FlexViewVertical>
+      <SectionList
+        sections={listData}
+        renderItem={({ item }) => (
+          <FlexViewHorizontal>
+            <LeftText>{item.accountName}</LeftText>
+            <RightText>
+              R$
+              {parseInt(item.accountBalance).toLocaleString('en-US', { style: 'currency', currency: 'USD' })}
+            </RightText>
+          </FlexViewHorizontal>
+        )}
+        renderSectionHeader={({ section }) => (
+          <HeaderText>{section.title}</HeaderText>
+        )}
+        keyExtractor={(item, index) => item.accountName}
+      />
+      <AddAccount onPress={Teste} title={"Add new account"} />
+    </FlexViewVertical>
+  );
 }
